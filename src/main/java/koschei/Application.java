4d3794e2413fb07eee471137e1c0d47a2b97479a
vlasikxd <nobel96@bk.ru>
2023-03-04 17:20:12 +0300

    2_1_5

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -8,8 +8,10 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         KoscheiTheDeathless koscheiTheDeathless =
                 applicationContext.getBean(KoscheiTheDeathless.class);
+
         System.out.println(koscheiTheDeathless.getRulesByDeth());
     }
 }
